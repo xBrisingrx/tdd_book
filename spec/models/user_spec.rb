ec50b33b6b1d004f2c9be9f22ea8c139e7ea6f47
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-20 17:34:01 -0300

    change user spec: I add a comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
-
+    # I valid that email be a correct format
     it { is_expected.to_not allow_value('foo@').for(:email) }
     it { is_expected.to_not allow_value('@bar.com').for(:email) }
   end

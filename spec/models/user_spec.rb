0e785324c26e88ce39596c45a0f62b71ebb5bb52
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-20 17:06:34 -0300

    change user spec: test validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe User, type: :model do
   it 'has a valid factory' do
     expect(user).to be_valid
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
 end

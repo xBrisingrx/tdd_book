ff32627a9e69f8dc9eb2ee94fe6e644f068eb844
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-23 19:56:16 -0300

    change home spec: test display posts

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 RSpec.describe 'Home' do
   it 'renders homepage' do
+    create(:page, :published)
     visit root_path
     within 'header' do
       expect(page).to have_link 'My Blog'
+    end
+
+    # find all find all of our <article> elements on the page
+    articles = find_all('article')
+    expect(articles.size).to eq(1)
+
+    within articles.first do
+      expect(page).to have_css('h2', text: Page.last.title)
     end
   end
 end

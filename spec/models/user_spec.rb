f8a9faafd9f247bd578ed3d12f1b8d58cb672f50
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-23 12:54:40 -0300

    ♻️ clean user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
-  it 'has a valid factory' do
-    expect(user).to be_valid
-  end
+  subject { build(:user) }
 
   describe 'validations' do
+    it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     # I valid that email be a correct format
+    it { is_expected.to allow_value('foo@bar.com').for(:email) }
     it { is_expected.to_not allow_value('foo@').for(:email) }
     it { is_expected.to_not allow_value('@bar.com').for(:email) }
   end

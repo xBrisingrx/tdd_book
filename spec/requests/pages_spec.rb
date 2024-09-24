d72a26daffac408cccd4076453f382140670a057
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-24 11:09:14 -0300

    fix page spec: we add a slug to the request

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   describe "GET /show" do
+    let(:page) { create(:page, :published) }
     it "returns http success" do
-      get "/pages/show"
+      get page_path(slug: page.slug)
       expect(response).to have_http_status(:success)
     end
   end

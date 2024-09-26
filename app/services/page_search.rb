a5d1a79f600d1c3618b906395ba78c2aae91895d
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-26 19:17:02 -0300

    fix page_search
    When params[:terms] == "" we return [], before did terurn nil
    The searh filter by published and order the search

--- app/services/page_search.rb
+++ app/services/page_search.rb
@@ -1,12 +1,14 @@
 module PageSearch
   def self.search(params)
     return [] unless params.present?
-    
-    Page.by_term(params[:term]) if params[:term].present?
-    
-    if params[:year].present? && params[:month].present?
-      Page.by_year_month(params[:year], params[:month])
+
+    pages = if params[:year].present? && params[:month].present?
+              Page.by_year_month(params[:year], params[:month])
+    elsif params[:term].present?
+              Page.by_term(params[:term])
     end
+    return [] unless pages
+    pages.published.ordered
   end
 end
 # Se crea un modulo en lugar de una clase xq no vamos a crear ninguna instancia
